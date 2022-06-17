e6c5ad3bfe129f1a10a0e33922b0410680d5226a
Panayiotis Halouvas <phalouvas@kainotomo.com>
2022-06-17 08:00:08 +0300

    removed migrations

--- html/app/Providers/Hubspot/src/Http/Controllers/HubspotController.php
+++ html/app/Providers/Hubspot/src/Http/Controllers/HubspotController.php
@@ -3,13 +3,27 @@
 namespace Smsto\Hubspot\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HubspotController extends Controller
 {
 
+    /**
+     * Component gui
+     *
+     * @author Panayiotis Halouvas <phalouvas@kainotomo.com>
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
     public function index(Request $request)
     {
+        $response = Http::get('https://integration.sms.to/component_bulk_sms/manifest.json');
+        $manifest = json_decode($response, true);
+        $url = route('home'); // to get Base Url
+        $VITE_ROUTE_PARAMS =  $url . "rest/V1/smsto-sms/params";
+        $VITE_ROUTE_SMSTO =  $url . "rest/V1/smsto-sms/callsmsto";
+        dd($VITE_ROUTE_PARAMS);
         return view('hubspot::hubspot');
     }
-
 }
