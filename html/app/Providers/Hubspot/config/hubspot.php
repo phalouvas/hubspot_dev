f7c87c261803f1dbe79ebc41c8bd94051688e147
Panayiotis Halouvas <phalouvas@kainotomo.com>
2022-06-29 07:16:32 +0300

    fix config APP_URL

--- html/app/Providers/Hubspot/config/hubspot.php
+++ html/app/Providers/Hubspot/config/hubspot.php
@@ -13,7 +13,7 @@ return [
             'payload' =>
             '
             {
-                "actionUrl": "' . env('HUBSPOT_APP_URL') . '/hubspot/smsto/send",
+                "actionUrl": "' . env('APP_URL') . '/hubspot/smsto/send",
                 "published": true,
                 "inputFields": [
                     {
